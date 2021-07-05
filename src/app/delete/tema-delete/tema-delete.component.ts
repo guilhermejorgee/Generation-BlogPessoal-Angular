52e814b26dea0e12a973fb8c54da1d15fb8c305c
Guilherme <guilherme.silva307@fatec.sp.gov.br>
2021-07-05 13:24:16 -0300

    implementando as funções de editar e deletar tema

---
+++ src/app/delete/tema-delete/tema-delete.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Tema } from 'src/app/model/Tema';
+import { TemaService } from 'src/app/service/tema.service';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-tema-delete',
+  templateUrl: './tema-delete.component.html',
+  styleUrls: ['./tema-delete.component.css']
+})
+export class TemaDeleteComponent implements OnInit {
+
+  tema: Tema = new Tema()
+
+  //idTema: number;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private temaService: TemaService
+  ) { }
+
+  ngOnInit(){
+
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+    }
+
+    let id = this.route.snapshot.params['id']
+
+    this.findTemaById(id)
+
+  }
+
+  findTemaById(id: number){
+
+    this.temaService.getById(id).subscribe((resp: Tema)=>{
+      this.tema = resp
+    })
+
+  }
+
+  deleteTema(){
+
+    this.temaService.deleteTema(this.tema.id).subscribe(()=>{
+      alert('Tema deletado com sucesso')
+      this.router.navigate(['/tema'])
+    })
+
+  }
+
+}
