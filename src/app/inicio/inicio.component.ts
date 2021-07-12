3f24b970ceb2f7af5505b592093da59012f339ae
Guilherme <guilherme.silva307@fatec.sp.gov.br>
2021-07-12 11:47:23 -0300

    alertas reutilizáveis implementados

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -4,6 +4,7 @@ import { environment } from 'src/environments/environment.prod';
 import { Postagem } from '../model/Postagem';
 import { Tema } from '../model/Tema';
 import { User } from '../model/User';
+import { AlertasService } from '../service/alertas.service';
 import { AuthService } from '../service/auth.service';
 import { PostagemService } from '../service/postagem.service';
 import { TemaService } from '../service/tema.service';
@@ -38,7 +39,8 @@ export class InicioComponent implements OnInit {
     private router: Router,
     private postagemService: PostagemService,
     private temaService: TemaService,
-    private auth: AuthService
+    private auth: AuthService,
+    private alertas: AlertasService
   ) { }
 
 
@@ -90,7 +92,7 @@ export class InicioComponent implements OnInit {
 
     this.findAllPostagens
 
-      alert('Postagem realizada com sucesso')
+      this.alertas.showAlertSuccess('Postagem realizada com sucesso')
     })
 
   }
